bc5d14d29708538fd045163ffc254fc11905ce9b
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:59:10 +0500

    docs: README faylini yangilash

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,12 +1,9 @@
 import { authGoogleController } from "@controllers";
 import { Router } from "express";
-
 let authRouter: Router = Router()
-
 authRouter
         .get("/login/page", authGoogleController.getLoginPage)
         .get("/google", authGoogleController.getAuthGoogle)
         .get("/google/callback", authGoogleController.getAuthGoogleCallback)
         .get("/get/user/page", authGoogleController.getUserPage)
-
 export default authRouter
