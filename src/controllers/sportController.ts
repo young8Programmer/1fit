08682a935c68f5177783b9da6f8107273991bdc9
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:58:18 +0500

    refactor: kod formatlash va tozalash

--- src/controllers/sportController.ts
+++ src/controllers/sportController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+// image optimization qo'shildi
 import { ErrorHandler } from "@errors";
 import { PrismaClient } from "@prisma/client";
 
