d7dc4a3afc0871b6f52f1d15cddfaf1ac3edcd38
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:59:17 +0500

    test: unit testlar qo'shildi

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,13 +4,10 @@ import adminRouter from "./adminRouter";
 import userRouter from "./userRouter";
 import gymsRouter from "./gymsRouter";
 import sportsRouter from "./sportsRouter";
-
 let router: Router = Router()
-
 router.use("/auth", authRouter)
 router.use("/users", userRouter)
 router.use("/gyms", gymsRouter)
 router.use("/sports", sportsRouter)
 router.use("/admin", adminRouter)
-
 export default router
