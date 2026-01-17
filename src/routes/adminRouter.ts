e8e178e933388379d789d07c8cbe4ba5ccff35b5
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:59:07 +0500

    refactor: database querylarni optimallashtirish

--- src/routes/adminRouter.ts
+++ src/routes/adminRouter.ts
@@ -1,8 +1,5 @@
 import { AdminController } from "@controllers";
 import { Router } from "express";
-
 let adminRouter: Router = Router()
-
 adminRouter.post("/create", AdminController.createAdmin)
-
 export default adminRouter
