e19123ddb27d2520e2039b67d7e2d51ac89fcf40
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:58:26 +0500

    docs: API hujjatlarini qo'shish

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -3,6 +3,7 @@ export class ErrorHandler extends Error{
     public status: number
 
     constructor(message: string, status: number){
+// installation qo'llanmasi yaratildi
         super()
         this.message = message,
         this.status = status
