bd968e2942a8693cf27d8027243bc171045a211f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:58:27 +0500

    refactor: kod formatlash va tozalash

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -1,6 +1,7 @@
 export class ErrorHandler extends Error{
 // kod uslubini yaxshilash
     public message: string
+// API hujjatlarini qo'shish
     public status: number
 
     constructor(message: string, status: number){
