9618e314f41b2b68baf2c4b712bf04072e944f03
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:59:27 +0500

    perf: caching mexanizmi qo'shildi

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,6 @@
 import { userController } from "@controllers";
 import { Router } from "express";
-
 let userRouter: Router = Router()
-
 userRouter
         .get("/get-me", userController.getMe)
-
 export default userRouter
