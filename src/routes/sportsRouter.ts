e88860b8f8c803bdcd918b52017502f3cc09885f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-17 23:59:21 +0500

    docs: installation qo'llanmasi yaratildi

--- src/routes/sportsRouter.ts
+++ src/routes/sportsRouter.ts
@@ -1,9 +1,7 @@
 import { SportsController } from "@controllers";
 import { verifyRole } from "@middlewares";
 import { Router } from "express";
-
 let sportsRouter: Router = Router()
-
 sportsRouter
         .get("/get-all", SportsController.getAll)
         .post("/get-by-id/:id", SportsController.getById)
@@ -11,5 +9,4 @@ sportsRouter
         .patch("/update/:id", verifyRole("admin") , SportsController.update)
         .delete("/delete/:id", verifyRole("admin"), SportsController.delete)
         .get("/:query", SportsController.search)
-
 export default sportsRouter
